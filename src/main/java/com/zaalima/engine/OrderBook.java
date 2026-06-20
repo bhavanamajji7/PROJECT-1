d4d7474d9a9c0dc7385704ec1a6e4555958aca7c
Ny8948 <168828850+Ny8948@users.noreply.github.com>
2026-06-20 22:11:22 +0530

    Update OrderBook.java

--- src/main/java/com/zaalima/engine/OrderBook.java
+++ src/main/java/com/zaalima/engine/OrderBook.java
@@ -1,21 +1,43 @@
 package com.zaalima.engine;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.TreeMap;
+import java.util.Collections;
 
 public class OrderBook {
 
-    private final List<String> orders = new ArrayList<>();
+    private String symbol;
+    private TreeMap<Double, PriceLevel> buyOrders;
+    private TreeMap<Double, PriceLevel> sellOrders;
 
-    public void addOrder(String order) {
-        orders.add(order);
+    public OrderBook(String symbol) {
+        this.symbol = symbol;
+        this.buyOrders = new TreeMap<>(Collections.reverseOrder());
+        this.sellOrders = new TreeMap<>();
     }
 
-    public List<String> getOrders() {
-        return orders;
+    public void addOrder(Order order) {
+        if (order.getSide().equals("BUY")) {
+            buyOrders.computeIfAbsent(
+                order.getPrice(),
+                k -> new PriceLevel(order.getPrice())
+            ).addOrder(order);
+        } else {
+            sellOrders.computeIfAbsent(
+                order.getPrice(),
+                k -> new PriceLevel(order.getPrice())
+            ).addOrder(order);
+        }
     }
 
-    public int size() {
-        return orders.size();
+    public TreeMap<Double, PriceLevel> getBuyOrders() {
+        return buyOrders;
+    }
+
+    public TreeMap<Double, PriceLevel> getSellOrders() {
+        return sellOrders;
+    }
+
+    public String getSymbol() {
+        return symbol;
     }
 }
