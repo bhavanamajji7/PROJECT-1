87c2a298f6923f48a9ad014a84208a7d9acccb46
THRISAIJUTTIGA <thrisai06@gmail.com>
2026-06-20 21:22:29 +0530

    Day 4: OrderEvent class for Disruptor added

---
+++ src/main/java/com/zaalima/engine/OrderEvent.java
@@ -0,0 +1,18 @@
+package com.zaalima.engine;
+
+public class OrderEvent {
+
+    private Order order;
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public void clear() {
+        this.order = null;
+    }
+}
