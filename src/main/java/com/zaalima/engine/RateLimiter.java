e67a9182c05f686d3117a7ef2a110896749d0ab8
Ny8948 <168828850+Ny8948@users.noreply.github.com>
2026-06-24 21:47:57 +0530

    Update RateLimiter.java

---
+++ src/main/java/com/zaalima/engine/RateLimiter.java
@@ -0,0 +1,21 @@
+package com.zaalima.engine;
+
+public class RateLimiter {
+
+    private long lastRequestTime;
+
+    public synchronized boolean allowRequest() {
+
+        long currentTime =
+                System.currentTimeMillis();
+
+        if (currentTime - lastRequestTime >= 1000) {
+
+            lastRequestTime = currentTime;
+
+            return true;
+        }
+
+        return false;
+    }
+}
