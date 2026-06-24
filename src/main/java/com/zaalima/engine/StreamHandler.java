84ced392f327b71c4ed8e0e8a8e0bb031ec3bfb6
Ny8948 <168828850+Ny8948@users.noreply.github.com>
2026-06-24 21:51:19 +0530

    Update StreamHandler.java

---
+++ src/main/java/com/zaalima/engine/StreamHandler.java
@@ -0,0 +1,10 @@
+package com.zaalima.engine;
+
+public class StreamHandler {
+
+    public void process(String event) {
+
+        System.out.println(
+                "Processing Event : " + event);
+    }
+}
