26ed29fd5950f07b7daaf4902fb037af2584d963
THRISAIJUTTIGA <thrisai06@gmail.com>
2026-06-29 23:43:00 +0530

    Day 15: JVMConfig for JVM tuning added

--- src/main/java/com/zaalima/engine/JVMConfig.java
+++ src/main/java/com/zaalima/engine/JVMConfig.java
@@ -1,4 +1,20 @@
 package com.zaalima.engine;
 
 public class JVMConfig {
+    public static void printJVMInfo() {
+        System.out.println("Java Version: " +
+                System.getProperty("java.version"));
+        System.out.println("Max Memory: " +
+                Runtime.getRuntime().maxMemory() / 1024 / 1024 + " MB");
+        System.out.println("Available Processors: " +
+                Runtime.getRuntime().availableProcessors());
+    }
+
+    public static long getAvailableMemory() {
+        return Runtime.getRuntime().freeMemory();
+    }
+
+    public static int getProcessors() {
+        return Runtime.getRuntime().availableProcessors();
+    }
 }
