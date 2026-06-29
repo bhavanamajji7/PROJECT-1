52c88f09db4b6610124de31744047c74c881398b
THRISAIJUTTIGA <thrisai06@gmail.com>
2026-06-29 23:43:43 +0530

    Day 18: IntegrationTest for end-to-end testing added

--- src/test/java/com/zaalima/engine/IntegrationTest.java
+++ src/test/java/com/zaalima/engine/IntegrationTest.java
@@ -1,4 +1,23 @@
 package com.zaalima.engine;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class IntegrationTest {
+    private MatchingEngine engine;
+
+    @BeforeEach
+    void setUp() {
+        engine = new MatchingEngine();
+    }
+
+    @Test
+    void testFullOrderFlow() {
+        Order buy = new Order("B1", "AAPL", "BUY", 150.0, 10);
+        Order sell = new Order("S1", "AAPL", "SELL", 150.0, 10);
+        engine.processOrder(buy);
+        var trades = engine.processOrder(sell);
+        assertFalse(trades.isEmpty());
+    }
 }
