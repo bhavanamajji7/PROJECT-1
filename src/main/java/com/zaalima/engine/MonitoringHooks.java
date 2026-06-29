66ec75102faf89c5dd5b29300645378ebe45d8b9
THRISAIJUTTIGA <thrisai06@gmail.com>
2026-06-29 23:43:32 +0530

    Day 17: MonitoringHooks for system monitoring added

--- src/main/java/com/zaalima/engine/MonitoringHooks.java
+++ src/main/java/com/zaalima/engine/MonitoringHooks.java
@@ -1,12 +1,37 @@
 package com.zaalima.engine;
 
 public class MonitoringHooks {
+    private long totalOrders;
+    private long totalTrades;
+    private long startTime;
 
-    public void recordMetric(
-            String metric,
-            double value) {
+    public MonitoringHooks() {
+        this.startTime = System.currentTimeMillis();
+    }
 
-        System.out.println(
-                metric + " = " + value);
+    public void recordOrder() {
+        totalOrders++;
+    }
+
+    public void recordTrade() {
+        totalTrades++;
+    }
+
+    public long getTotalOrders() {
+        return totalOrders;
+    }
+
+    public long getTotalTrades() {
+        return totalTrades;
+    }
+
+    public long getUptimeSeconds() {
+        return (System.currentTimeMillis() - startTime) / 1000;
+    }
+
+    public void printStats() {
+        System.out.println("Orders: " + totalOrders);
+        System.out.println("Trades: " + totalTrades);
+        System.out.println("Uptime: " + getUptimeSeconds() + "s");
     }
 }
